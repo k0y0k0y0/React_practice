3b0aeb9c980f04ac9f49a8e227dae30d32a62673
taak <papipupepo_10@yahoo.co.jp>
2020-08-27 09:39:17 +0900

    done

--- pixabay/src/App.js
+++ pixabay/src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/ImageList';
+import logo from './pixabay_logo.png';
 
 const App=()=>{
+  const[images, setImages] = useState([]);
+  const Apikey = process.env.REACT_APP_PIXABAY_APIKEY;
+  const onSearchSubmit=async(term)=>{
+    try{
+      const params={
+        key: Apikey,
+        q: term,
+      };
+      const response= await axios.get("https://pixabay.com/api/", {params});
+      setImages(response.data.hits);
+      if(response.data.total===0){
+        window.alert('お探しの画像はありません。');
+      }
+    }catch{
+      window.alert('写真の取得に失敗しました。');
+    }
+  };
+
   return(
-    <div>
-      <p>
-        App
-      </p>
+    <div className='ui container' style={{margin: '20px'}}>
+      <img src={logo} alt='pixabay-logo' className='pixabay-logo' />
+      <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
 };
