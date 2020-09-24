524b0052f5ce070d7c02e9e7e821013a6944a017
taak <papipupepo_10@yahoo.co.jp>
2020-09-25 08:24:30 +0900

    Modify itemCreater

--- redux_form/src/components/items/ItemCreate.js
+++ redux_form/src/components/items/ItemCreate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Field, reduxForm, formValues } from 'redux-form';
+import { Field, reduxForm} from 'redux-form';
 
 class ItemCreate extends React.Component{
   renderError({error, touched}){
