236c718dfc923d1ba452909ac5fae67d7691cd52
taak <papipupepo_10@yahoo.co.jp>
2020-10-07 08:36:04 +0900

    Done

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Aoo from './components/app';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
